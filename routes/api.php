ea2dda7419998210d7fdfb081c30591c795ec8f3
Oscar <o.martin9203@gmail.com>
2019-05-16 23:01:45 -0400

    add .env

--- routes/api.php
+++ routes/api.php
@@ -17,4 +17,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 //Route::apiResource('productos', 'ProductController');
-Route::middleware('auth:api')->resource('/productos', 'ProductController');
+//Route::middleware('auth:api')->resource('/productos', 'ProductController');
