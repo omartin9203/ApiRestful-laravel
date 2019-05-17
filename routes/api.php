4c8aed33b60b516cfdc5e969aaee48be21fef10e
Oscar <o.martin9203@gmail.com>
2019-05-16 23:04:11 -0400

    fix not

--- routes/api.php
+++ routes/api.php
@@ -16,5 +16,5 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
-//Route::apiResource('productos', 'ProductController');
+Route::apiResource('productos', 'ProductController');
 Route::middleware('auth:api')->resource('/productos', 'ProductController');
